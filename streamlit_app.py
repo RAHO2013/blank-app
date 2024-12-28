3aab5cde56ddb4abd55cb23e35f6c12a73774456
RAHO2013 <myranktech.ppt@gmail.com>
2024-12-28 21:53:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,17 @@ import streamlit as st
 st.set_page_config(page_title="ETERNALS", layout="wide")
 st.title("Welcome to ETERNALS")
 
-# Remove sidebar navigation
-st.sidebar.title("ETERNALS Navigation")
-st.sidebar.info("Use the links on the home page to navigate.")
+st.sidebar.title("Navigation")
+st.sidebar.radio("Select a page:", [
+    "Master Data",
+    "Order Creation",
+    "Order Creation with Excel",
+    "Order Comparison",
+    "Fee Checking"
+])
 
 st.markdown("""
 ## Home Page
-Welcome to **ETERNALS**! Please use the navigation menu below to select a section.
-
-### Sections:
-- [Master Data](Master%20Data)
-- [Order Creation](Order%20Creation)
-- [Order Creation with Excel](Order%20Creation%20with%20Excel)
-- [Order Comparison](Order%20Comparison)
-- [Fee Checking](Fee%20Checking)
+Welcome to **ETERNALS**!
+Use the sidebar to navigate to different sections.
 """)
