322cc317bf753dbde4e22e0f8f250a266043e78d
RAHO2013 <myranktech.ppt@gmail.com>
2024-12-28 22:38:41 +0530

    Delete pages directory

--- pages/comparison.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Order Comparison")
-st.write("Comparison functionality coming soon.")
-
--- pages/fee_checking.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Fee Checking")
-st.write("Fee checking functionality coming soon.")
